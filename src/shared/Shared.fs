c8856fd7a15e35e2f6163e27a066cb440ec858ab
Gyalus Gergő <gyalus.gergo.98@gmail.com>
2023-11-07 13:44:56 +0100

    leftover

--- src/shared/Shared.fs
+++ src/shared/Shared.fs
@@ -1,6 +1,16 @@
 ﻿namespace WsReactExample.Shared
 open WebSharper
 
+[<JavaScript>]
+type FundraiserInfoServer = {
+    Name:string
+    FundraiserLink: JavaScript.URL option
+    Owner: {|
+        Name:string
+    |}
+    OwnerLink: JavaScript.URL option
+    Goal: float
+}
 type IApi = {
     [<Remote>] GetValue : unit -> Async<string>
 
